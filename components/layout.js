3fd89208d06739d0120aa31dc670cd4b4fa6405b
Alex Beattie <artrisanbranding@me.com>
2020-10-23 11:12:27 -0700

    updated

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Alex Beattie'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Work in Progress'
 
 export default function Layout({ children, home }) {
     return (
